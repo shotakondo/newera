f6e75cdfac6f1b386a607572bb9f243a51ad697c
kondoshota <35793862+kondoshota@users.noreply.github.com>
2019-02-14 16:41:27 +0900

    a

--- kondo/teain/WEB-INF/classes/dao/OraStockDao.java
+++ kondo/teain/WEB-INF/classes/dao/OraStockDao.java
@@ -1,11 +1,11 @@
-package dao;
-
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
+package dao;				
+						
+import java.sql.DriverManager;			
+import java.sql.PreparedStatement;		
+import java.sql.ResultSet;				
+import java.sql.Connection;		
+import java.sql.SQLException;		
+import java.util.ArrayList;										
 import java.util.List;
 
 import exp.ResourceAccessException;
@@ -18,77 +18,66 @@ public class OraStockDao implements StockDao{
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		
-		int num = 0;
-		
-		try{
+		int num = 0;		try{
 			//select文
 			String sql = "SELECT  stock_amount FROM stock_table WHERE stock_product_id = '" + pid + "'";
 			
 			//PreparedStatementインターフェイスを実装するクラスの
 			//インスタンスを取得する
 			st = cn.prepareStatement(sql);
-			
-			//select文を送信し
+						//select文を送信し
 			//ResultSetインターフェイスを実装したクラスの
 			//インスタンスが返る
 			rs = st.executeQuery();
-			
 			//カーソルを一行ずつスクロールし、データをフェッチする
 			rs.next();
-			
-			num =  Integer.parseInt(rs.getString(1));
+						num =  Integer.parseInt(rs.getString(1));
 			
 		//getConnection, prepareStatement, executeQueryで例外発生の場合
 		}catch(SQLException e){
 			//ロールバックする
-			try{
-				cn.rollback();
+			try{	cn.rollback();
 			}catch(SQLException e2){
 				//独自例外にラップして送出する
-				throw new ResourceAccessException(e2.getMessage(), e2);
-			}
+throw new ResourceAccessException(e2.getMessage(), e2);		}
 			
 			//独自例外にラップして送出する
 			throw new ResourceAccessException(e.getMessage(), e);
-			
 		}
 		return num;
+	}	public void setStock(int num, String pid){
 	}
-	public void setStock(int num, String pid){
-	}
-	
-	public void alterStock(int num, String pid){
+		public void alterStock(int num, String pid){
 		
 		Connection cn = OracleConnectionManager.getInstance().getConnection();
 		PreparedStatement st = null;
-		
-		try{
+			try{
 			//select文
 			String sql = "update stock_table set stock_amount = (stock_amount - ?) where stock_product_id = ?";
-			
+		
 			//PreparedStatementインターフェイスを実装するクラスの
 			//インスタンスを取得する
-			st = cn.prepareStatement(sql);
+	st = cn.prepareStatement(sql);
 			
 			//パラメータをセットする
-			st.setInt(1, num);
-			st.setString(2, pid);
+		st.setInt(1, num);
+		st.setString(2, pid);
 			
 			st.executeUpdate();
-			
+	
 		//getConnection, prepareStatement, executeQueryで例外発生の場合
-		}catch(SQLException e){
-			//ロールバックする
-			try{
-				cn.rollback();
-			}catch(SQLException e2){
-				//独自例外にラップして送出する
-				throw new ResourceAccessException(e2.getMessage(), e2);
-			}
-			
+	}catch(SQLException e){
+		//ロールバックする
+		try{
+			cn.rollback();
+		}catch(SQLException e2){
 			//独自例外にラップして送出する
-			throw new ResourceAccessException(e.getMessage(), e);
-			
-		}
+				throw new ResourceAccessException(e2.getMessage(), e2);
 	}
+		
+		//独自例外にラップして送出する
+		throw new ResourceAccessException(e.getMessage(), e);
+		
+	}
+}
 }
--- kondo/teain/WEB-INF/classes/dao/OraUserDao.java
+++ kondo/teain/WEB-INF/classes/dao/OraUserDao.java
@@ -15,94 +15,85 @@ import beans.User;
 public class OraUserDao implements UserDao{
 	
 	public void createUser(UserBean ub){
-		
-		Connection cn = null;
+						Connection cn = null;
 		PreparedStatement st = null;
-		
+
 		try{
-			cn = OracleConnectionManager.getInstance().getConnection();
+		cn = OracleConnectionManager.getInstance().getConnection();
 			
-			String sql = "insert into user_table values(?,?,?,?,?,?,?,?,?,?)";
-			
-			st = cn.prepareStatement(sql);
-			
-			st.setString(1,ub.getId());
-			st.setString(2,ub.getFirstName());
-			st.setString(3,ub.getLastName());
-			st.setString(4,ub.getEmail());
-			st.setString(5,ub.getPass());
+	String sql = "insert into user_table values(?,?,?,?,?,?,?,?,?,?)";
+		
+		st = cn.prepareStatement(sql);
+	
+		st.setString(1,ub.getId());
+st.setString(2,ub.getFirstName());
+	st.setString(3,ub.getLastName());
+		st.setString(4,ub.getEmail());
+	st.setString(5,ub.getPass());
 			st.setString(6,ub.getTel());
-			st.setString(7,ub.getPostcode());
-			st.setString(8,ub.getAddress());
+		st.setString(7,ub.getPostcode());
+	st.setString(8,ub.getAddress());
 			st.setString(9,ub.getSex());
-			st.setString(10,ub.getBirthday());
-			
+			st.setString(10,ub.getBirthday());			
 			st.executeUpdate();
-			
 		}catch(SQLException e){
-			
 			e.printStackTrace();
 			
-		}finally{
-			try{
-				if(st != null){
-					st.close();
-					st = null;
-				}
+	}finally{
+	try{
+		if(st != null){
+				st.close();
+		st = null;
+	}
 			}catch(SQLException e){
 				e.printStackTrace();
-			}
-		}
+}
 	}
-	
-	public boolean checkLogin(UserBean ub){
+}
+		public boolean checkLogin(UserBean ub){
 		
 		ResultSet rs =null;
 		Connection cn = null;
-		PreparedStatement ps = null;
-		
+	PreparedStatement ps = null;	
 		boolean judge = false;
-		
-		try {
-			cn = OracleConnectionManager.getInstance().getConnection();
-			String sql = "select * from user_table where user_email='" + ub.getEmail() + "'and user_pass='" + ub.getPass()+"'";
+
+		try {		cn = OracleConnectionManager.getInstance().getConnection();
+	String sql = "select * from user_table where user_email='" + ub.getEmail() + "'and user_pass='" + ub.getPass()+"'";
 			
-			ps = cn.prepareStatement(sql);
+		ps = cn.prepareStatement(sql);
 			rs = ps.executeQuery();
-			
+	
 			while(rs.next()){
-				ub.setUid(rs.getString(1));
+	ub.setUid(rs.getString(1));
 				ub.setEmail(rs.getString(2));
-				ub.setFirstName(rs.getString(4));
+		ub.setFirstName(rs.getString(4));
 				ub.setLastName(rs.getString(5));
-				ub.setTel(rs.getString(6));
+		ub.setTel(rs.getString(6));
 				ub.setPostcode(rs.getString(7));
-				ub.setAddress(rs.getString(8));
-				ub.setSex(rs.getString(9));
+	ub.setAddress(rs.getString(8));			ub.setSex(rs.getString(9));
 				ub.setBirthday(rs.getString(10));
-			}
+	}
 			
-			if(rs.getString(2).equals(ub.getEmail()) && rs.getString(3).equals(ub.getPass())){
-				judge=true;
-			}else{
-				judge=false;
-			}
+	if(rs.getString(2).equals(ub.getEmail()) && rs.getString(3).equals(ub.getPass())){
+	judge=true;			}else{
+		judge=false;
+	}
 		}catch(SQLException e){
 			e.printStackTrace();
-		}finally{
+}finally{
 			try{
-				if(ps != null){
-					ps.close();
-					ps = null;
+	if(ps != null){
+				ps.close();
+				ps = null;
 				}
 			}catch(SQLException e){
-				e.printStackTrace();
+			e.printStackTrace();
 			}
-		}
-		return judge;
-	}
+}
+	return judge;
+}
 	
-	public UserBean getUser(String uid){
+public UserBean getUser(String uid){
 		
 		PreparedStatement st = null;
 		Connection cn = null;
@@ -115,239 +106,181 @@ public class OraUserDao implements UserDao{
 			
 			String sql = "SELECT * from user_table where user_id='" + id + "'";
 			
-			st = cn.prepareStatement(sql);
-			
-			rs = st.executeQuery();
+								st = cn.prepareStatement(sql);
+							rs = st.executeQuery();
 			
 			rs.next();
-			
-			
+					
 			ub.setId(rs.getString(1));
 			ub.setFirstName(rs.getString(2));
 			ub.setLastName(rs.getString(3));
 			ub.setEmail(rs.getString(4));
 			ub.setPass(rs.getString(5));
 			ub.setTel(rs.getString(6));
-			ub.setPostcode(rs.getString(7));
-			ub.setAddress(rs.getString(8));
-			ub.setSex(rs.getString(9));
-			ub.setBirthday(rs.getString(10));
+ub.setPostcode(rs.getString(7));
+	ub.setAddress(rs.getString(8));
+	ub.setSex(rs.getString(9));
+	ub.setBirthday(rs.getString(10));
 			
-		}catch(SQLException e){
-			OracleConnectionManager.getInstance().rollback();
+		}catch(SQLException e){	OracleConnectionManager.getInstance().rollback();
 			e.printStackTrace();
 			throw new ResourceAccessException(e.getMessage(), e);
 		}finally{
-			try{
-				if(st!=null){
-					st.close();
-				}
-			}catch(SQLException e){
-				e.printStackTrace();
+			try{				if(st!=null){
+		st.close();
+		}
+			}catch(SQLException e){		e.printStackTrace();
 				throw new ResourceAccessException(e.getMessage(), e);
 			}
 		}
 		return ub;
 	}
-	
 	public void DeleteUser(String uid){
-		
 		Connection cn = null;
 		PreparedStatement st = null;
 		
-		boolean judge = false;
-		
-		try{
-			cn = OracleConnectionManager.getInstance().getConnection();
-			
+		boolean judge = false;try{
+			cn = OracleConnectionManager.getInstance().getConnection();			
 			String sql = "DELETE from user_table where user_id='"+uid+"'";
 			
 			st = cn.prepareStatement(sql);
-			
-			st.executeUpdate();
-			
+			st.executeUpdate();			
 		}catch(SQLException e){
 			OracleConnectionManager.getInstance().rollback();
 		}finally{
-			try{
-				if(st!=null){
-					st.close();
+			try{				if(st!=null){
+			st.close();
 				}
 			}catch(SQLException e){
-				e.printStackTrace();
-			}
-		}
+			e.printStackTrace();
 	}
-	
+	}
+}
 	public boolean checkEmail(UserBean ub){
 		
-		ResultSet rs =null;
-		Connection cn = null;
-		PreparedStatement ps = null;
+	ResultSet rs =null;
+	Connection cn = null;		PreparedStatement ps = null;
 		
 		boolean judge = false;
 		
-		try {
-			cn = OracleConnectionManager.getInstance().getConnection();
-			String sql = "select user_id,user_email,user_tel from user_table where user_email='" + u.getEmail() + "'and user_tel='" + u.getTel()+"'";
-			ps = cn.prepareStatement(sql);
+		try {	cn = OracleConnectionManager.getInstance().getConnection();
+			String sql = "select user_id,user_email,user_tel from user_table where user_email='" + u.getEmail() + "'and user_tel='" + u.getTel()+"'";	ps = cn.prepareStatement(sql);
 			rs = ps.executeQuery();
 			
-			rs.next()
+			rs.next();
 			ub.setUid(rs.getString(1));
-			
-			if(rs.getString(2).equals(ub.getEmail()) && rs.getString(3).equals(ub.getTel())){
+						if(rs.getString(2).equals(ub.getEmail()) && rs.getString(3).equals(ub.getTel())){
 				judge=true;
 			}
 			else{
-				judge=false;
-			}
-			
-		}catch(SQLException e){
+				judge=false;}
+					}catch(SQLException e){
 			
 			e.printStackTrace();
 		}finally{
-			try{
-				if(ps != null){
+			try{	if(ps != null){
 					ps.close();
-					ps = null;
-				}
+	ps = null;			}
 			}catch(SQLException e){
-				e.printStackTrace();
+	e.printStackTrace();
 			}
-		}
-		return judge;
 	}
-	
+	return judge;
+}
 	public void OracleUpdate(String sql){
 		Connection cn = null;
-		PreparedStatement st = null;
-		try{
+		PreparedStatement st = null;		try{
 			cn = OracleConnectionManager.getInstance().getConnection();
 			st = cn.prepareStatement(sql);
-			st.executeUpdate();
-		}catch(SQLException e){
+			st.executeUpdate();		}catch(SQLException e){
 			OracleConnectionManager.getInstance().rollback();
 			e.printStackTrace();
-		}finally{
-			try{
+		}finally{			try{
 				if(st!= null){
 					st.close();
-				}
-			}catch(SQLException ex){
+				}			}catch(SQLException ex){
 				ex.printStackTrace();
-			}
-		}
-	}
-	
+			}		}
+	}	
 	public void EditUser(UserBean ub){
 		
-		PreparedStatement st = null;
-		Connection cn = OracleConnectionManager.getInstance().getConnection();
-		
-		
-		try{
+	PreparedStatement st = null;
+	Connection cn = OracleConnectionManager.getInstance().getConnection();
 			
-			String sql = "UPDATE user_table SET user_firstname = ?, user_lastname = ?, user_email = ?, user_tel = ?, user_postcode = ?, user_address = ?, user_sex = ?, user_birthday = ? where user_id = '"+ ub.getUid() + "'";
-			
-			st = cn.prepareStatement(sql);
-
-			st.setString(1,ub.getFirsname());
+	try{			String sql = "UPDATE user_table SET user_firstname = ?, user_lastname = ?, user_email = ?, user_tel = ?, user_postcode = ?, user_address = ?, user_sex = ?, user_birthday = ? where user_id = '"+ ub.getUid() + "'";
+				st = cn.prepareStatement(sql);			st.setString(1,ub.getFirsname());
 			st.setString(2,ub.getLastname());
 			st.setString(3,ub.getEmail());
-			st.setString(4,ub.getTel());
-			st.setString(5,ub.getPostcode());
+			st.setString(4,ub.getTel());			st.setString(5,ub.getPostcode());
 			st.setString(6,ub.getAddress());
-			st.setString(7,ub.getSex());
-			st.setString(8,ub.getBirthday());
-			
-			st.executeUpdate();
+			st.setString(7,ub.getSex());			st.setString(8,ub.getBirthday());			st.executeUpdate();
 			
 		}catch(SQLException e){
-			
-			try{
+		try{
 				cn.rollback();
-			}catch(SQLException e2){
+		}catch(SQLException e2){
 				throw new ResourceAccessException(e2.getMessage(), e2);
 			}
-			throw new ResourceAccessException(e.getMessage(), e);
+	throw new ResourceAccessException(e.getMessage(), e);
 		}
 	}
 	
-	public void PasswordReplace(UserBean ub){
+public void PasswordReplace(UserBean ub){
 		
-		PreparedStatement st = null;
-		ResultSet rs = null;
+PreparedStatement st = null;
+	ResultSet rs = null;
 		Connection cn = null;
-		
-		try{
-			cn = OracleConnectionManager.getInstance().getConnection();
+				try{
+		cn = OracleConnectionManager.getInstance().getConnection();
 			
-			String sql = "UPDATE user_table set user_pass = ? where user_id = ?";
+	String sql = "UPDATE user_table set user_pass = ? where user_id = ?";
 			
 			st = cn.prepareStatement(sql);
-			
-			st.setString(1,ub.getPass());
-			st.setString(2,ub.getUid());
-			
-			st.executeUpdate();
-			
+		
+		st.setString(1,ub.getPass());
+		st.setString(2,ub.getUid());
+		
+	st.executeUpdate();		
 		}catch(SQLException e){
 			
 			e.printStackTrace();
 			b = false;
 		}finally{
-			try{
-				if(rs != null){
-					rs.close();
-				}
-				if(st != null){
-					st.close();
-				}
-			}catch(SQLException e){
+			try{				if(rs != null){
+					rs.close();}
+				if(st != null){					st.close();
+				}			}catch(SQLException e){
 				e.printStackTrace();
 			}
-		}
-	}
-	
-	public boolean CheckPassword(UserBean ub){
+		}	}
+		public boolean CheckPassword(UserBean ub){
 		
 		ResultSet rs =null;
-		Connection cn = null;
-		PreparedStatement st = null;
+	Connection cn = null;	PreparedStatement st = null;
 			
-		boolean judge= false;
-		
-		try {
+		boolean judge= false;		try {
 			cn = OracleConnectionManager.getInstance().getConnection();
 			String sql = "select user_id,user_email,user_pass from user_table where user_id='" + ub.getId() + "' and user_email='" + ub.getEmail() + "'and user_pass='" + ub.getPass()+"'";
 			
-			st = cn.prepareStatement(sql);
-			
-			rs = st.executeQuery();
+			st = cn.prepareStatement(sql);			rs = st.executeQuery();
 			
 			rs.next();
 			
 			ub.setUid(rs.getString(1));
 			
 			if(rs.getString(2).equals(ub.getEmail()) && rs.getString(3).equals(ub.getPass())){
-				
-				judge = true;
-			}else{
-				judge = false;
-			}
-		}catch(SQLException e){
+								judge = true;
+			}else{judge = false;
+			}		}catch(SQLException e){
 			e.printStackTrace();
 		}finally{
 			try{
 				if(st != null){
-					st.close();
-					st = null;
+					st.close();		st = null;
 				}
 			}catch(SQLException e){
 				e.printStackTrace();
 			}
-		}
-		return judge;
+		}		return judge;
 	}
 }
