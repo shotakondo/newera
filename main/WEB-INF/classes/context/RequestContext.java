546829fa610544d411da97e617e369fa343bc4b8
kondoshota <35793862+kondoshota@users.noreply.github.com>
2019-02-19 15:17:26 +0900

    a

--- main/WEB-INF/classes/context/RequestContext.java
+++
@@ -1,9 +0,0 @@
-package context;
-
-public interface RequestContext{
-	public String getCommandPath();
-	public String[] getParameter(String key);
-	public Object getRequest();
-	public void setRequest(Object request);
-	Object getSessionObject(String object);
-}
