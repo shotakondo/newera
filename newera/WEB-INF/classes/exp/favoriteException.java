5793175939981fcf7ac0359a9677f7c8ed88caa5
TOKYO <theanh3789@gmail.com>
2019-02-28 16:38:12 +0900

    Update 02.28 2回目
    もうちょっと終わるだよー

--- newera/WEB-INF/classes/exp/favoriteException.java
+++
@@ -1,8 +0,0 @@
-package exp;
-
-public class favoriteException extends RuntimeException{
-	public favoriteException(String mess,Throwable cause){
-		super(mess,cause);
-		System.out.println("favoriteExceptionの中に来たよ");
-	}
-}
